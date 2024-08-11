743aa7cbc35c9fc1582ff281044e97280f1144cd
Jaswantsuthar <95173617+Jaswantsuthar@users.noreply.github.com>
2024-08-11 10:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+function promise(value,timeOut) {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(value)
+		},timeOut)
+	})
+}
+
+async function calling() {
+    let value = document.getElementById("text").value
+    let timeOut = parseInt(document.getElementById("delay").value);
+
+
+	let call = await promise(value,timeOut)
+	document.getElementById("output").textContent =call
+    console.log(call)
+}
+
+
+document.getElementById("btn").addEventListener("click", calling);
